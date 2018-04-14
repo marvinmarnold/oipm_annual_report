05837c69864fe8548c6252e00585a6e883db3801
Marvin Arnold <marvin@geeky.rocks>
2018-04-14 10:45:18 -0500

    Reorganize for github pages

--- primary_sources/iapro/complaints_misconduct_master.R
+++ primary_sources/iapro/complaints_misconduct_master.R
@@ -17,10 +17,6 @@ allegations.for.year <- complaints.alleg.act.for.year %>%
   select(FIT.Number, Officer.Race, Disposition.OIPM, Officer.primary.key, Allegation, Allegation.class, Officer.sex) %>% 
   distinct()
 
-distinct(al)
-
-allegations.for.year %>% select(FIT.Number, Citizen.primary.key)
-
 complaints.all <- complaints.misconduct.all %>% 
   filter(Year.occurred == year, Incident.type == "Citizen Initiated")
 
@@ -32,39 +28,3 @@ misconduct.alleg.act <- rbind(complaints.alleg.act.for.year, misconduct.alleg.ac
 # Print disposition normalization
 dispositions <- unique(misconduct.alleg.act %>% select(Status, Disposition.Reported, Disposition.OIPM, Disposition.NOPD))
 # write.csv(dispositions, "disposition_conversion.csv")
-
-unique(dispositions$Disposition.OIPM)
-# Dispositions
-sustained, not sustained, exonerated, unfounded, withdrawn / mediated, NFIM, Illigitimate outcomes, DI2, pending
-
-[1] NFIM                         Unclear data                                           
-[5] Pending                                       NSA  -> Negotiated settlement                        Negotiated Settlement       
-[9] Resigned under investigation 
-
-# Allegation finding
-[1]                          NFIM CASE -> NFIM
-NO VIOLATIONS OBSERVED   -> Unfounded
-NFOUNDED               -> Unfounded
-DI-2                -> DI-2    
-[6] NOT SUSTAINED      
-EXONERATED       
-SUSTAINED          
-PENDING INVESTIGATION   
-WITHDRAWN               -> illigitimate
-[11] WITHDRAWN- MEDIATION    
-CANCELLED                -> ill
-NEGOTIATED SETTLEMENT    -> sustained
-DUPLICATE ALLEGATION    -> ill
-RECLASSIFIED AS DI-2    -> DI-2
-[16] DUPLICATE INVESTIGATION -> ill
-SUSTAINED - RUI - RETIRE -> ill
-SUSTAINED - Deceased  -> sustained
-NOT SUSTAINED - RUI  -> not sustained
-RUI-Resigned Under Inves -> ill 
-[21] Sustained - Dismissed  
-sustained               
-REDIRECTION           -> di2
-AWAITING HEARING     -> sustained
-Exonerated 
-
-if any allegation sutained, case sutained
--- reports/AnnualReport2017/README.md
+++
@@ -1,8 +0,0 @@
-# OIPM 2017 Annual Report
-
-## Getting started
-library(rsconnect)
-rsconnect::deployApp('reports/AnnualReport2017/')
-
-## Dependencies
-shiny, shinydashboard, dplyr, tidyr, ggplot2
