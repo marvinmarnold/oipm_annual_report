18b8d83b3cfdddb28ce07f4961034e3a697c5aad
Marvin Arnold <marvin@geeky.rocks>
2018-04-16 07:42:36 -0500

    Fix links from main page

--- analysis/bwc/bwc.R
+++ analysis/bwc/bwc.R
@@ -16,22 +16,23 @@ epr.2017.csv <- "data/data.nola.gov/Electronic_Police_Report_2017.csv"
 
 ########################################################################################################
 ######################################## GLOBAL VARIABLES ##############################################
-
+epr.2017 <- read.csv(epr.2017.csv)
+colnames(epr.2017)
 warning("This file is over 400GB and will take a long time to load into memory from CSV")
 bwc.w.officer.2017 <- read.csv(bwc.w.officer.2017.csv)
 bwc.w.officer.2017 <- bwc.w.officer.2017 %>% mutate(
   id_external = as.factor(id_external)
 )
-epr.2017 <- read.csv(epr.2017.csv)
-levels(epr.2017$Item_Number)
-levels(bwc.w.officer.2017$id_external)
-epr.2017 %>% mutate(
-  Item_Number = as.factor(Item_Number),
-  should.be.on = nrow(bwc.w.officer.2017[bwc.w.officer.2017$id_external == Item_Number,]) > 0
-)
 
-bwc.w.officer.2017$
-colnames(bwc.w.officer.2017)
-colnames(epr.2017)
+# To make command below a little faster
+bwc.with.legible.id <- bwc.w.officer.2017 %>% 
+  filter((nchar(as.character(id_external)) == 10)) %>%
+  select(id_external) %>%
+  distinct
 
-nrow(bwc.w.officer.2017 %>% filter(id_external == "H-10721-17")) > 0
+# FIXME this is horrendously inefficient
+epr.has.bwc <- sapply(epr.2017$Item_Number, function(epr_id) {
+  bwc.with.legible.id %>% filter(as.character(id_external) == as.character(epr_id)) %>% nrow > 0
+})
+
+
