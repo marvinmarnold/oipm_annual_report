444e819c76fe2a7ea10f1078576ef8503751eafb
Marvin Arnold <marvin@geeky.rocks>
2018-04-15 21:11:34 -0500

    Add a map of where officers live by zip

---
+++ analysis/officers/officer_zips.R
@@ -0,0 +1,71 @@
+library(leaflet)
+library(maptools)
+check.vars(c("active.officers.for.year"))
+title <- "Officers by zip"
+
+center.lat <- -90.078634
+center.long <- 29.942234
+zoom <- 9 # higher numbers are more zoomed in
+
+bins <- c(0, 1, 2, 5, 10, 20, 50, 75, 100, Inf)
+  
+########################################################################################################
+########################################################################################################
+# Count officers by zip
+officers.with.zip <- active.officers.for.year %>% filter(!is.na(Zip))
+officers.by.zip <- officers.with.zip %>% group_by(Zip)
+count.by.zip <- summarize(officers.by.zip, num.officers = n())
+
+# Load LA zip geojson
+louisiana <- geojsonio::geojson_read("data_public/louisiana.json", what = "sp")
+la.zips <- louisiana@data$ZCTA5CE10
+length(louisiana@data$ZCTA5CE10)
+#names(louisiana)
+
+count.la.zips <- sapply(la.zips, function(zip) {
+  nrow(officers.with.zip[officers.with.zip$Zip == zip,])
+})
+
+# Create new data frame with all zips in LA and count of officers in each (lots of 0s)
+la.zips.with.counts <- data.frame(
+  ZCTA5CE10 = la.zips,
+  officers.in.zip = count.la.zips
+)
+
+geo.zips.with.counts <- spCbind(louisiana, la.zips.with.counts$officers.in.zip)
+
+head(geo.zips.with.counts@data)
+# Color bins according to how many officers there are
+pal <- colorBin("YlOrRd", domain = geo.zips.with.counts$la.zips.with.counts.officers.in.zip, bins = bins)
+
+m <- leaflet(geo.zips.with.counts) %>%
+  setView(center.lat, center.long, zoom) %>%
+  addProviderTiles("MapBox", options = providerTileOptions(
+    id = "mapbox.light",
+    accessToken = Sys.getenv('MAPBOX_TOKEN')))
+
+labels <- sprintf(
+  "<strong>%s officers</strong> in %s",
+  geo.zips.with.counts$la.zips.with.counts.officers.in.zip,
+  geo.zips.with.counts$ZCTA5CE10
+) %>% lapply(htmltools::HTML)
+
+p.officer.map <- m %>% addPolygons(
+  fillColor = ~pal(la.zips.with.counts.officers.in.zip),
+  weight = 2,
+  opacity = 1,
+  color = "white",
+  dashArray = "3",
+  fillOpacity = 0.7,
+  highlight = highlightOptions(
+    weight = 5,
+    color = "#666",
+    dashArray = "",
+    fillOpacity = 0.7,
+    bringToFront = TRUE),
+  label = labels,
+  labelOptions = labelOptions(
+    style = list("font-weight" = "normal", padding = "3px 8px"),
+    textsize = "15px",
+    direction = "auto"))
+
