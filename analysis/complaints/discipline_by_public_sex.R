703fa6bdfcff42fb19c5c55a7af6209794a801e3
Marvin Arnold <marvin@geeky.rocks>
2018-05-30 13:17:51 -0500

    Discipline by officer race and gender

--- analysis/complaints/discipline_by_public_sex.R
+++ analysis/complaints/discipline_by_public_sex.R
@@ -1,8 +1,8 @@
 check.vars(c("year", "allegations.for.year"))
-title <- "Discipline by complainant's sex"
 
 ########################################################################################################
 ########################################################################################################
+title <- "Discipline by complainant's sex"
 
 complainant.info <- allegations.all %>% select(Citizen.sex, Allegation.primary.key) %>% distinct()
 detailed.actions.for.year <- merge(discipline.for.year, complainant.info, by = "Allegation.primary.key", all.x = TRUE)
@@ -11,9 +11,9 @@ discipline.by.sex <- detailed.actions.for.year %>% group_by(Action.taken.OIPM, C
 discipline.count.by.sex <- summarize(discipline.by.sex, num.allegations = n())
 
 p.discipline.by.sex <- plot_ly(discipline.count.by.sex, 
-                                      x = ~Action.taken.OIPM, y = ~num.allegations, 
-                                      type = 'bar',  name = ~Citizen.sex, 
-                                      color = ~Citizen.sex) %>%
+                               x = ~Action.taken.OIPM, y = ~num.allegations, 
+                               type = 'bar',  name = ~Citizen.sex, 
+                               color = ~Citizen.sex) %>%
   
   layout(xaxis = list(title = "Type of allegation", 
                       showgrid = F), 
@@ -23,3 +23,27 @@ p.discipline.by.sex <- plot_ly(discipline.count.by.sex,
          margin = list(r = 100, b = 100))
 
 p.discipline.by.sex
+
+########################################################################################################
+########################################################################################################
+title <- "Discipline by officers's sex"
+
+complainant.info <- allegations.all %>% select(Officer.sex, Allegation.primary.key) %>% distinct()
+detailed.actions.for.year <- merge(discipline.for.year, complainant.info, by = "Allegation.primary.key", all.x = TRUE)
+
+discipline.by.sex <- detailed.actions.for.year %>% group_by(Action.taken.OIPM, Officer.sex)
+discipline.count.by.sex <- summarize(discipline.by.sex, num.allegations = n())
+
+p.discipline.by.officer.sex <- plot_ly(discipline.count.by.sex, 
+                               x = ~Action.taken.OIPM, y = ~num.allegations, 
+                               type = 'bar',  name = ~Officer.sex, 
+                               color = ~Officer.sex) %>%
+  
+  layout(xaxis = list(title = "Type of allegation", 
+                      showgrid = F), 
+         yaxis = list(title = 'Number allegations'), 
+         barmode = 'stack',
+         hovermode = 'compare', 
+         margin = list(r = 100, b = 100))
+
+p.discipline.by.officer.sex
--- analysis/complaints/discipline_by_race.R
+++ analysis/complaints/discipline_by_race.R
@@ -23,3 +23,27 @@ p.discipline.by.race <- plot_ly(discipline.count.by.race,
          margin = list(r = 100, b = 100))
 
 p.discipline.by.race
+
+########################################################################################################
+########################################################################################################
+title <- "Discipline by officer's race"
+
+complainant.info <- allegations.all %>% select(Officer.Race, Allegation.primary.key) %>% distinct()
+detailed.actions.for.year <- merge(discipline.for.year, complainant.info, by = "Allegation.primary.key", all.x = TRUE)
+
+discipline.by.race <- detailed.actions.for.year %>% group_by(Action.taken.OIPM, Officer.Race)
+discipline.count.by.race <- summarize(discipline.by.race, num.allegations = n())
+
+p.discipline.by.officer.race <- plot_ly(discipline.count.by.race, 
+                                x = ~Action.taken.OIPM, y = ~num.allegations, 
+                                type = 'bar',  name = ~Officer.Race, 
+                                color = ~Officer.Race) %>%
+  
+  layout(xaxis = list(title = "Type of allegation", 
+                      showgrid = F), 
+         yaxis = list(title = 'Number allegations'), 
+         barmode = 'stack',
+         hovermode = 'compare', 
+         margin = list(r = 100, b = 100))
+
+p.discipline.by.officer.race
